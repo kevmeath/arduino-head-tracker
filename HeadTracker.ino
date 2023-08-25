9c01020a775dbf3a2cfb9e146a921bd5db181d89
Kevin Meath <kevmeath@gmail.com>
2023-08-25 01:52:08 +0100

    Send UDP packets

--- HeadTracker.ino
+++ HeadTracker.ino
@@ -1,22 +1,25 @@
 #include <LSM6DS3.h>
-
 #include <WiFiNINA.h>
-
 #include <MadgwickAHRS.h>
-
 #include "secrets.h"
 
 #define SAMPLE_RATE 104.0f
 
 Madgwick filter;
 
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
+const char ssid[] = SECRET_SSID;
+const char pass[] = SECRET_PASS;
+const char ipStr[] = SECRET_IP;
+IPAddress ip;
+const uint16_t port = SECRET_PORT;
 int wifi_status = WL_IDLE_STATUS;
+WiFiUDP udp;
 
 float accX, accY, accZ;
 float gyroX, gyroY, gyroZ;
 float yaw, pitch, roll;
+
+double *packetData = (double *)calloc(6, sizeof(double));
 
 void setup()
 {
@@ -27,7 +30,7 @@ void setup()
     {
         Serial.print("Connecting to network: ");
         Serial.println(ssid);
-        
+
         wifi_status = WiFi.begin(ssid, pass);
 
         delay(10000);
@@ -41,11 +44,13 @@ void setup()
         while (true);
     }
 
+    ip.fromString(ipStr);
+    udp.begin(port);
     filter.begin(SAMPLE_RATE);
 }
 
 void loop()
-{   
+{
     if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable())
     {
         IMU.readAcceleration(accX, accY, accZ);
@@ -57,11 +62,19 @@ void loop()
         pitch = filter.getPitch();
         roll = filter.getRoll();
 
-        Serial.print("Yaw: ");
-        Serial.println(yaw);
-        Serial.print("Pitch: ");
-        Serial.println(pitch);
-        Serial.print("Roll: ");
-        Serial.println(roll);
+        packetData[3] = (double)yaw;
+        packetData[4] = (double)pitch;
+        packetData[5] = (double)roll;
+
+        for (int i = 0; i < 6; i++)
+        {
+            Serial.print(packetData[i]);
+            Serial.print(',');
+        }
+        Serial.println();
+
+        udp.beginPacket(ip, port);
+        udp.write((byte *)packetData, 6 * sizeof(double));
+        udp.endPacket();
     }
 }
